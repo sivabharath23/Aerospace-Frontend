cb34a1b9263b74f7a5c1680b7f00eadaf1a4ba0d
sivabharath23 <sivabharath23@gmail.com>
2023-07-15 16:22:30 +0530

    Uploading Website

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let x =(a,b) => {
+    return a+b;
+}
+
+console.log(x(1,2));
+
+
+let call1 = () => alert('hey there');
+
+call1();
+
+let call = () => alert('hey there');
+call(); // Invoking the function
