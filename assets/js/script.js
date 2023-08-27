221c771f7cb44cc95cd8d62db0de084a005a5b18
sivabharath23 <sivabharath23@gmail.com>
2023-08-27 09:15:04 +0530

    update on Aug-27-2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-let x =(a,b) => {
-    return a+b;
-}
+// let x =(a,b) => {
+//     return a+b;
+// }
 
-console.log(x(1,2));
+// console.log(x(1,2));
 
 
-let call1 = () => alert('hey there');
+// let call1 = () => alert('hey there');
 
-call1();
+// call1();
 
-let call = () => alert('hey there');
-call(); // Invoking the function
+// let call = () => alert('hey there');
+// call(); // Invoking the function
+
+
+window.onscroll = function() {
+    var menu = document.querySelector(".head-menu");
+    if (window.pageYOffset > 0) {
+        menu.classList.add("sticky");
+    } else {
+        menu.classList.remove("sticky");
+    }
+};
